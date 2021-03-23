d12cebc7aaee0a718dd0d2c19f3cf5baa88fb967
github username <d24dust@hotmail.com>
2021-03-23 13:01:30 -0400

    feat: helper method for rendering errors

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def run_block_or_render_error(custom_msg=nil)
+    begin
+      yield
+    rescue => exception
+      render json: {error: custom_msg ? "#{custom_msg}:  #{exception}" : "#{exception}", backtrace: exception.backtrace.first(5)}, status: 569
+    end
+  end
 end
