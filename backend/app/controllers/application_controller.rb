02901711456143bb8b1406dd85bf095f718c0507
github username <d24dust@hotmail.com>
2021-03-23 14:20:28 -0400

    feat: render model validation errors helper method; change: spacing

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < ActionController::API
+
   def run_block_or_render_error(custom_msg=nil)
     begin
       yield
     rescue => exception
-      render json: {error: custom_msg ? "#{custom_msg}:  #{exception}" : "#{exception}", backtrace: exception.backtrace.first(5)}, status: 569
+      render json: {
+        error: custom_msg ? "#{custom_msg}:  #{exception}" : "#{exception}", 
+        backtrace: exception.backtrace.first(5)
+      }, status: 569
+    end
+  end
+
+  def validate_model_with_block_or_render_error(model)
+    begin
+      yield
+    rescue => exception
+      render json: {
+        validation_error: "true", 
+        error_details: {
+          full_messages: model.errors.full_messages,
+          messages: model.errors.messages
+        }
+      }, status: 569
     end
   end
 end
