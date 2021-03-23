a3c5f40719fb7e2bcc98d0d77f24c8883d951b51
github username <d24dust@hotmail.com>
2021-03-23 17:03:50 -0400

    feat: show action

--- backend/app/controllers/coffee_tables_controller.rb
+++ backend/app/controllers/coffee_tables_controller.rb
@@ -1,2 +1,10 @@
 class CoffeeTablesController < ApplicationController
+
+  def show
+    run_block_or_render_error do
+      coffee_table = User.find(params[:user_id]).coffee_table
+      render json: CoffeeTableSerializer.new(coffee_table).to_serialized_json
+    end
+  end
+
 end
