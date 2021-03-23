bf0a0fe3af72d37b75998a9298db603fc3a5f6fb
github username <d24dust@hotmail.com>
2021-03-23 17:03:13 -0400

    feat: serializes to json

--- backend/app/serializers/coffee_table_serializer.rb
+++ backend/app/serializers/coffee_table_serializer.rb
@@ -1,5 +1,20 @@
 class CoffeeTableSerializer
-  def initialize
-    
+
+  def initialize(coffee_table, options=nil)
+    @coffee_table = coffee_table
   end
+
+  def to_serialized_json
+    options = {
+      only: [:id, :nickname],
+      include: {
+        user: {
+          only: [:id, :name, :email]
+        }
+      }
+    }
+
+    @coffee_table.to_json(options)
+  end
+
 end
