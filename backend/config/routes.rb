8aa030f3ee5e066424269ec2d1be007857edd3dc
github username <d24dust@hotmail.com>
2021-03-23 16:13:40 -0400

    add: nested coffee_table route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :create]
+  resources :users, only: [:show, :create] do
+    get "coffee_table", to: 'coffee_tables#show'
+  end
 end
