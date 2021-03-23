f07bb66bca4f9d6d2c8fa7e71d8c3ff567566273
github username <d24dust@hotmail.com>
2021-03-23 13:47:44 -0400

    feat: show action

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def show
-
+    run_block_or_render_error do
+      user = User.find(params[:id])
+      render json: UserSerializer.new(user).to_serialized_json
+    end
   end
   def create
-    
+
   end
 end
