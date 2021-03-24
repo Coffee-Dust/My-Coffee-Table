fba6ce654df31a9fe053bb90c75b215d39db0961
github username <d24dust@hotmail.com>
2021-03-24 19:53:20 -0400

    add: POST login route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :create] do
     get "coffee_table", to: 'coffee_tables#show'
