db147d5fe642cd14eaa21d5442a0127840417012
github username <d24dust@hotmail.com>
2021-03-24 09:48:28 -0400

    add: background_type to coffee_tables

---
+++ backend/db/migrate/20210324134010_add_background_type_to_coffee_table.rb
@@ -0,0 +1,5 @@
+class AddBackgroundTypeToCoffeeTable < ActiveRecord::Migration[6.0]
+  def change
+    add_column :coffee_tables, :background_type, :string
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_184038) do
+ActiveRecord::Schema.define(version: 2021_03_24_134010) do
 
   create_table "coffee_tables", force: :cascade do |t|
     t.string "nickname"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "background_type"
   end
 
   create_table "users", force: :cascade do |t|
