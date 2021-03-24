8fa185704154315db8fd84da5482fa68ee3c624a
github username <d24dust@hotmail.com>
2021-03-24 19:56:42 -0400

    feat: addLoginFormListener class method

--- frontend/js/user.js
+++ frontend/js/user.js
@@ -1,3 +1,20 @@
 class User {
 
+  static addLoginFormListener() {
+    document.querySelector("#login_form").addEventListener("submit", (event)=>{
+      event.preventDefault()
+
+      new AjaxCall("/login").postData({email: event.target.user_email.value}, (error)=>{
+        event.target.classList.toggle("field_with_errors")
+        event.target.user_email.placeholder = "Could not find account!"
+        setTimeout(function() {
+          event.target.classList.toggle("field_with_errors")
+          event.target.user_email.placeholder = "Enter your email."
+        }, 69000)
+
+      }).then(user=> new User(user).login() )
+
+    })
+  }
+  
 }
