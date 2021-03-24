0bd5e02d6113519479bef6a8aa0f574676303a31
github username <d24dust@hotmail.com>
2021-03-24 18:27:53 -0400

    add: postData method

--- frontend/js/util/ajax.js
+++ frontend/js/util/ajax.js
@@ -1,9 +1,37 @@
 class AjaxCall {
+  
   constructor(url) {
     this.url = url
   }
-  
+
+  postData(body, failedValidationCallback) {
+    const config = {
+      method: "POST",
+      headers: { 
+      "Content-Type": "application/json", 
+      "Accept": "application/json"
+      },
+      body: JSON.stringify(body)
+    }
+    this.submittedObject = body
+
+    return fetch(`${self.baseURL}${this.url}`, config).then(resp=>resp.json()).then(response=>{
+      this.returnedObject = response
+
+      if (this.returnedObject.errors) {
+        // Object failed validation:
+        console.error(`The data <${this.submittedObject}> has a validation error: `); console.error(this)
+        failedValidationCallback(this.returnedObject)
+        return Object.assign({}, this.returnedObject, {ajaxCall: this})
+      } else {
+        // It saved to the database!
+        return this.returnedObject
+      }
+    })
+  }
+
   getData() {
     return fetch(`${self.baseURL}${this.url}`).then(resp=>resp.json()).then(json=>this.returnedObject = json)
   }
+
 }
