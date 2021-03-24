7a9543dd6198294bd8ef75f086d03af208367d50
github username <d24dust@hotmail.com>
2021-03-24 19:53:41 -0400

    feat: create action

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
-
+  def create
+    if user = User.find_by(email: params[:email])
+      render json: UserSerializer.new(user).to_serialized_json
+    else
+      render json: {
+        errors: {
+          full_messages: ["Could not find user with that email!"]
+        }
+      }
+    end
+  end
 end
