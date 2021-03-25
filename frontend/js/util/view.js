bab658359a0457f1de2961a3ff83f926db0cce3b
github username <d24dust@hotmail.com>
2021-03-25 17:59:58 -0400

    feat: presentPopup function

--- frontend/js/util/view.js
+++ frontend/js/util/view.js
@@ -1,3 +1,11 @@
 class View {
-  
+  static presentPopup(callback) {
+    const popup = document.body.appendChild(document.createElement('div'))
+    popup.id = "popupMenu"
+    popup.style.height = window.innerHeight
+    const popupBox = popup.appendChild(document.createElement('div'))
+
+    callback(popupBox)
+  }
+
 }
