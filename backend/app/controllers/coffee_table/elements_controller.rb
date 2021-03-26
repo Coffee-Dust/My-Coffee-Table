50b62d527123d8ea59be44ce79dcc008d3e9eb53
github username <d24dust@hotmail.com>
2021-03-26 16:59:07 -0400

    feat: index action

--- backend/app/controllers/coffee_table/elements_controller.rb
+++ backend/app/controllers/coffee_table/elements_controller.rb
@@ -1,3 +1,11 @@
 class CoffeeTable::ElementsController < ApplicationController
+  def index
+
+    run_block_or_render_error do
+      elements = User.find(params[:user_id]).coffee_table.elements
+      render json: ElementSerializer.new(elements).to_serialized_json
+    end
+
+  end
 
 end
