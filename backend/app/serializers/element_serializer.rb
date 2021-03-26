050eea860633acf78055d1f360fc837bf3e1b8fb
github username <d24dust@hotmail.com>
2021-03-26 16:59:25 -0400

    feat: now serializes :D

--- backend/app/serializers/element_serializer.rb
+++ backend/app/serializers/element_serializer.rb
@@ -1,3 +1,20 @@
 class ElementSerializer
-  
+  def initialize(element, options=nil)
+    @element = element
+  end
+
+  def to_serialized_json
+    options = {
+      only: [:id, :className, :elementable_type],
+      include: {
+        style: {
+          except: [:id, :created_at, :updated_at]
+        },
+        elementable: {
+          except: [:id, :created_at, :updated_at]
+        }
+      }
+    }
+    @element.to_json(options)
+  end
 end
