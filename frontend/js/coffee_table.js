8b042a0b532bb87ce927ad8efe6965dca22a3b9d
github username <d24dust@hotmail.com>
2021-03-26 13:38:23 -0400

    add: loadForUser class method

--- frontend/js/coffee_table.js
+++ frontend/js/coffee_table.js
@@ -1,3 +1,10 @@
 class CoffeeTable {
-  
+
+  static loadForUser(user) {
+    //load new view controller for user's coffeetable and assign to singleton that was declared on global/index.js
+    new AjaxCall(`/users/${user.id}/coffee_table`).getData().then(coffeeTable=> {
+      self.coffeeTableViewConroller = new CoffeeTableViewConroller(new CoffeeTable(coffeeTable))
+    })
+  }
+
 }
