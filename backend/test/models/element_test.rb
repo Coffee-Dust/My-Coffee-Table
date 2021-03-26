27f9fbe289080848845fbb2413979daf1d1373da
github username <d24dust@hotmail.com>
2021-03-26 11:56:20 -0400

    add: tests

--- backend/test/models/element_test.rb
+++ backend/test/models/element_test.rb
@@ -4,4 +4,13 @@ class ElementTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Can have polymorphic child" do
+    c = CoffeeTable.create!(user: User.create!(name: "lol", email: "idk"))
+    e = c.elements.build()
+    l = FancyLink.create!(url:"face")
+    s = e.build_style(cssText: "lol")
+    e.elementable = l
+    c.save!
+    assert e.elementable.url == "face"
+  end
 end
