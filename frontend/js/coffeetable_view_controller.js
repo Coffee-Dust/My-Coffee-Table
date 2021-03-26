4e6f755a55adfb6998cbe469d005b2b93318f7ab
github username <d24dust@hotmail.com>
2021-03-26 16:19:57 -0400

    add: constructor & getter setter for coffeetable

--- frontend/js/coffeetable_view_controller.js
+++ frontend/js/coffeetable_view_controller.js
@@ -1,3 +1,20 @@
 class CoffeeTableViewController {
 
+  constructor(coffeeTable) {
+    this.coffeeTable = coffeeTable
+  }
+
+  set coffeeTable(tableClass) {
+    if (tableClass.constructor.name === "CoffeeTable") {
+      this._coffeeTable = tableClass
+    } else {
+      console.log(this)
+      new Error(`Tried to set an instance of ${tableClass.constructor.name} to this.coffeeTable`)
+    }
+  }
+
+  get coffeeTable() {
+    return this._coffeeTable
+  }
+
 }
