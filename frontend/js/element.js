09b3f06a631717ca487c5aec5ab60749886ff4ca
github username <d24dust@hotmail.com>
2021-03-26 17:19:12 -0400

    add: class constructor

--- frontend/js/element.js
+++ frontend/js/element.js
@@ -1,3 +1,9 @@
 class Element {
+  constructor(elementData) {
+    this.id = elementData.id
+    this.elementableType = elementData.elementable_type
+    this.className = elementData.className
+    this.style = elementData.style
+  }
   
 }
