276d2f03d06f014af91d1079c57ca30a8141d642
github username <d24dust@hotmail.com>
2021-03-26 18:39:08 -0400

    add: createNode method

--- frontend/js/element.js
+++ frontend/js/element.js
@@ -3,6 +3,15 @@ class Element {
     this.data = data
     this.data.elementableType = data.elementable_type
 
-    // this.node = this.createNode()
+    this.node = this.createNode()
+  }
+
+  createNode() {
+    const node = document.createElement('div')
+    node.id = `${this.data.elementableType}_${this.data.id}`
+    node.className = this.data.className
+    node.style.cssText = this.data.style.cssText
+    // node.appendChild(this.createElementableNode())
+    return node
   }
 }
