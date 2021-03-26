3d5cff30e5fefae53c59118ef5654e98342ece18
github username <d24dust@hotmail.com>
2021-03-26 14:14:19 -0400

    feat: constructor

--- frontend/js/coffee_table.js
+++ frontend/js/coffee_table.js
@@ -1,9 +1,26 @@
 class CoffeeTable {
 
+  constructor(tableData) {
+
+    if(!tableData.id) {
+      console.error("Did not receive valid CoffeeTable data for User: " + self.currentUser.id)
+      console.error(tableData)
+      new Error("Invalid Coffeetable Data")
+    } 
+
+    this.id = tableData.id
+    this.nickname = tableData.nickname
+    this.backgroundType = tableData.background_type
+    this.elements = []
+
+    for (const element of tableData.elements) {
+      this.elements.push(new Element(element))
+    }
+  }
+
   static loadForUser(user) {
-    //load new view controller for user's coffeetable and assign to singleton that was declared on global/index.js
     new AjaxCall(`/users/${user.id}/coffee_table`).getData().then(coffeeTable=> {
-      self.coffeeTableViewConroller = new CoffeeTableViewConroller(new CoffeeTable(coffeeTable))
+      self.coffeeTableViewController = new CoffeeTableViewController(new CoffeeTable(coffeeTable))
     })
   }
 
