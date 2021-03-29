55108fa4c799f116bc02bdd7e66f7478a2590428
github username <d24dust@hotmail.com>
2021-03-29 13:19:35 -0400

    add: functional edit button

--- frontend/js/coffeetable_view_controller.js
+++ frontend/js/coffeetable_view_controller.js
@@ -3,16 +3,34 @@ class CoffeeTableViewController {
   constructor(coffeeTable) {
     this.coffeeTable = coffeeTable
     this.view = document.querySelector("#coffee_table_view")
+    this.editModeIsActive = false
 
     if (!this.view) {
       throw new Error(`Could not find HTMLElement #coffee_table_view`)
     }
+    this.displayEditButton()
   }
 
   loadElements() {
     for (const element of this.coffeeTable.elements) {
       this.view.appendChild(element.node)
     }
+  }
+
+  displayEditButton() {
+    const btn = document.createElement("button")
+    btn.id = "edit_button"; btn.textContent = "Edit Mode"
+    btn.addEventListener("click", (event)=>{
+      if (this.editModeIsActive) {
+        this.editModeIsActive = false
+        event.target.textContent = "Edit Mode"
+      } else {
+        this.editModeIsActive = true
+        event.target.textContent = "Exit Edit Mode"
+      }
+      
+    })
+    this.view.appendChild(btn)
   }
 
   set coffeeTable(tableClass) {
