13c5da7f843d6d783e55c7dcaac96ea35f63cdbf
github username <d24dust@hotmail.com>
2021-03-30 15:52:43 -0400

    add: nested attributes for style, elementable

--- backend/app/models/element.rb
+++ backend/app/models/element.rb
@@ -2,4 +2,12 @@ class Element < ApplicationRecord
   belongs_to :coffee_table
   belongs_to :style
   belongs_to :elementable, polymorphic: true
+
+  accepts_nested_attributes_for :style
+
+  def elementable_attributes=(data)
+    attributes = data.reject { |d| d == "type" }
+    self.elementable = data["type"].constantize.new(attributes)
+  end
+  
 end
