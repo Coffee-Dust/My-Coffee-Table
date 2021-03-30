3fd4e43ab6c8d9f624b5a4c5a7f0c321a908666c
github username <d24dust@hotmail.com>
2021-03-30 15:05:43 -0400

    refactor: formCreation method

--- frontend/js/elementable.js
+++ frontend/js/elementable.js
@@ -14,25 +14,11 @@ class FancyLink {
     return a
   }
 
-  static creationForm() {
-    const form = document.createElement("form")
-    form.innerHTML = `
-    <input type="hidden" name="elementable[type]" value="FancyLink">
-    <input type="text" name="elementable[url]" placeholder="Website Link">
-    <input type="text" name="elementable[textContent]" placeholder="Text To Display">
-    <input type="submit">
+  static generateFormInputs() {
+    return `
+    <input type="hidden" name="type" value="FancyLink" class="elementable">
+    <input type="text" name="url" placeholder="Website Link" class="elementable">
+    <input type="text" name="textContent" placeholder="Text To Display" class="elementable">
     `
-    form.addEventListener("submit", (event) => {
-      event.preventDefault()
-      const formData = {}
-      console.log(event.target)
-
-      for (const input of event.target.elements) {
-        console.log(input.name)
-        formData[input.name] = input.value
-      }
-      console.log(formData)
-    })
-    return form
   }
 }
