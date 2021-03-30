73c499aad5a94755f8b6e8fb62cef773c7e2ab02
github username <d24dust@hotmail.com>
2021-03-30 16:57:58 -0400

    feat: create action

--- backend/app/controllers/coffee_table/elements_controller.rb
+++ backend/app/controllers/coffee_table/elements_controller.rb
@@ -8,4 +8,26 @@ class CoffeeTable::ElementsController < ApplicationController
 
   end
 
+  def create
+    element = Element.new(element_params)
+    validate_model_with_block_or_render_error(element) do
+      if element.save!
+        render json: ElementSerializer.new(element).to_serialized_json
+      end
+    end
+  end
+
+  private
+
+  def element_params
+    type = params[:element][:elementable_type]
+    params.require(:element).permit(
+      :coffee_table_id,
+      :className,
+      :elementable_type,
+      style_attributes: [:cssText],
+      elementable_attributes: type.constantize.attribute_names
+    )
+  end
+
 end
