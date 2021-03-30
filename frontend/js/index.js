5a3f70a6de3a133d183b05c25ea3eadabfdf992d
github username <d24dust@hotmail.com>
2021-03-30 15:59:06 -0400

    fix: add element namespace to postData on #create

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,3 +2,25 @@ self.baseURL = "http://localhost:3000"
 
 User.addListeners()
 CoffeeTableViewController.fixTableSize()
+
+document.querySelector("#header_bar").addEventListener("mouseover", (event)=>{
+  if (event.target.querySelector("nav")) {
+    event.target.querySelector("nav").hidden = false
+    event.target.classList.remove("animate_close")
+  }
+})
+
+document.querySelector("#header_bar").addEventListener("mouseout", (event) => {
+  if (self.currentUser) {
+    event.target.querySelector("nav").hidden = true
+    event.target.classList.add("animate_close")
+  }
+})
+
+function headerBarHide(event) {
+  
+}
+
+function headBarShow(params) {
+  
+}
