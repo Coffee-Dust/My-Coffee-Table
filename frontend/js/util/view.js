a5817fb6c52c97ed10a6631e863515a50bba91ea
github username <d24dust@hotmail.com>
2021-03-31 21:25:31 -0400

    add: DragEvent.dragStart method & event listener

--- frontend/js/util/view.js
+++ frontend/js/util/view.js
@@ -27,6 +27,21 @@ class DragEvent {
     this.positionsDefault = {
       offset: { x: null, y: null }
     }
+    this.view.addEventListener("mousedown", (e)=>this.dragStart(e))
+    // this.view.addEventListener("mousemove", (e)=>this.onDrag(e))
+    // this.view.addEventListener("mouseup", (e)=>this.dragEnd(e))
+  }
+
+  dragStart(event) {
+    const node = event.target
+
+    if (node.classList.contains("edit_bar") && this.controller.editModeIsActive) {
+      this.element = this.controller.coffeeTable.elements.find((element)=> element.node.id === node.parentNode.id)
+      this.active = true
+
+      this.positions.offset.x = event.clientX - this.element.position.x
+      this.positions.offset.y = event.clientY - this.element.position.y
+    }
   }
 
 }
