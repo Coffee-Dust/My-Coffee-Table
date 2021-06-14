00bbce4d4d933f45d08911d634d4f96f6f28a55f
github username <d24dust@hotmail.com>
2021-06-14 14:41:34 -0400

    add: iframe test

--- backend/test/models/element_test.rb
+++ backend/test/models/element_test.rb
@@ -13,4 +13,14 @@ class ElementTest < ActiveSupport::TestCase
     c.save!
     assert e.elementable.url == "face"
   end
+
+  test "Can have polymorphic iframe" do 
+    c = CoffeeTable.create!(user: User.create!(name: "lol", email: "idk"))
+    e = c.elements.build()
+    i = FancyIframe.create!(src: "something google maybe")
+    s = e.build_style(cssText: "lol")
+    e.elementable = i
+    c.save!
+    assert e.elementable.src == "something google maybe"
+  end
 end
