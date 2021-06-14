9c934832a7344e42771f77b7de0da9bf0f9687af
github username <d24dust@hotmail.com>
2021-06-14 15:02:20 -0400

    add: FancyIframe.generateFormInputs()

--- frontend/js/elementable.js
+++ frontend/js/elementable.js
@@ -29,4 +29,16 @@ class FancyIframe {
 
   }
 
+  static generateFormInputs() {
+    return `
+      <input type="hidden" name="type" value="FancyIframe" class="elementable">
+      <input type="text" name="title" placeholder="Frame Title" class="elementable" />
+      <input type="text" name="caption" placeholder="Frame Caption" class="elementable" />
+      <input type="text" name="src" placeholder="Frame Website Source" class="elementable" />
+
+      <input type="number" name="width" placeholder="Frame Size Width" class="elementable" />
+      <input type="number" name="height" placeholder="Frame Size Height" class="elementable" />
+    `
+  }
+
 }
