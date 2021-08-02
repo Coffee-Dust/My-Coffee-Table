3d68040c9d8bab1061dc7fcb284534798a6fe282
github username <d24dust@hotmail.com>
2021-08-02 11:26:44 -0400

    remove: unreliable script loading function

--- public/js/onload.js
+++ public/js/onload.js
@@ -1,22 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  consoleLoadMessage()
-  loadScriptFile("util/ajax.js")
-  loadScriptFile("util/view.js")
-  loadScriptFile("user.js")
-  loadScriptFile("coffeetable_view_controller.js")
-  loadScriptFile("coffee_table.js")
-  loadScriptFile("element.js")
-  loadScriptFile("elementable.js")
-
-  loadScriptFile("index.js")
-})
-
-function loadScriptFile(filename) {
-  console.log(`Loading Script: ${filename}`)
-  var script = document.createElement('script');
-  script.src = "./js/" + filename;
-  document.body.appendChild(script)
-}
 
 function consoleLoadMessage() {
 
